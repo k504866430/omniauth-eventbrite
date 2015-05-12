7583af7558eb4c194070b497e7e75ba4299db5a9
Michael Herold <michael.j.herold@gmail.com>
2015-05-12 08:33:40 -0500

    Add some more documentation

--- lib/omniauth/strategies/eventbrite.rb
+++ lib/omniauth/strategies/eventbrite.rb
@@ -8,6 +8,10 @@ module OmniAuth
       INFO_URL = 'https://www.eventbriteapi.com/v3/users/me/'.freeze
 
       option :name, 'eventbrite'
+
+      # The list of optional values that can be sent with authorize
+      #
+      # @note :ref is a referral code for the Referral Program.
       option :authorize_options, [:ref]
       option :client_options, site: 'https://www.eventbrite.com',
                               authorize_url: '/oauth/authorize',
@@ -27,6 +31,8 @@ module OmniAuth
       end
 
       # The OAuth2 client authorization parameters.
+      #
+      # @return [OmniAuth::Strategy::Options]
       def authorize_params
         super.tap do |params|
           params[:response_type] ||= DEFAULT_RESPONSE_TYPE
@@ -42,6 +48,8 @@ module OmniAuth
       end
 
       # The OAuth2 client authentication parameters.
+      #
+      # @return [OmniAuth::Strategy::Options]
       def token_params
         super.tap do |params|
           params[:grant_type] ||= DEFAULT_GRANT
